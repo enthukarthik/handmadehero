176e9566372a9a2cd3a623c0bf1bb9b9f2357346
Karthik Rathinavelu <r.karthik@outlook.com>
2025-01-30 18:37:45 +0530

    Adding some comments to build.bat and shell.bat

--- code/shell.bat
+++ code/shell.bat
@@ -1,4 +1,7 @@
 @echo off
 
+REM Call VS batch file to enable cl.exe available in the path
 call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
+
+REM Add handmade hero path where we can execute the build executables and batch scripts
 set PATH=W:\code;W:\build;%PATH%
