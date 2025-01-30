426a6a169f356bd4e3ca1efbdb7787f7723fbc6b
Karthik Rathinavelu <r.karthik@outlook.com>
2025-01-30 18:31:05 +0530

    Adding build.bat and shell.bat. Initial WinMain

---
+++ code/build.bat
@@ -0,0 +1,5 @@
+@echo off
+
+pushd ..\build
+cl /Zi ..\code\win32_handmade.cpp
+popd
---
+++ code/win32_handmade.cpp
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int WinMain(
+    HINSTANCE hInstance,
+    HINSTANCE hPrevInstance,
+    LPSTR     lpCmdLine,
+    int       nShowCmd
+)
+{
+    return 0;
+}
