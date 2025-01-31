58b1f1b22ede398d395e97eb3898e33fe7cb8ad3
Karthik Rathinavelu <r.karthik@outlook.com>
2025-01-31 18:57:17 +0530

    Day 2 : Creating a simple window

--- code/win32_handmade.cpp
+++ code/win32_handmade.cpp
@@ -1,5 +1,36 @@
 #include <windows.h>
 
+LRESULT Wndproc(
+    HWND hWnd,
+    UINT uMsg,
+    WPARAM wParam,
+    LPARAM lParam
+)
+{
+    switch(uMsg)
+    {
+        case WM_CREATE:
+            OutputDebugString(TEXT("Window created\n"));
+            break;
+
+        case WM_CLOSE:
+            // This will add WM_DESTROY to the queue
+            OutputDebugString(TEXT("Close button is pressed\n"));
+            break;
+
+        // WM_DESTROY msg gets passed when the close button is pressed
+        case WM_DESTROY:
+            OutputDebugString(TEXT("WM_DESTROY msg processed\n"));
+            PostQuitMessage(0);                         // This will post WM_QUIT msg which will exit the event loop
+            break;
+
+        default:
+            break;
+    }
+
+    return DefWindowProc(hWnd, uMsg, wParam, lParam);
+}
+
 int WinMain(
     HINSTANCE hInstance,
     HINSTANCE hPrevInstance,
@@ -7,6 +38,45 @@ int WinMain(
     int       nShowCmd
 )
 {
-    MessageBox(0, "Handmade Hero is a beast", "Handmade Hero", MB_OK | MB_ICONINFORMATION);
-    return 0;
+    WNDCLASS wndClass {};
+    wndClass.style       = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;  // CS_OWNDC : Allocate own DC for every window created through this class
+    wndClass.lpfnWndProc = Wndproc;                             // The windows proc to be called
+    wndClass.hInstance   = hInstance;                           // hInstance of the module that contains the WndProc. GetModuleHandle(0) should do the same
+    wndClass.lpszClassName = TEXT("HHWndClass");                // A name for this class. TEXT() to make it work for both ASCII and Unicode
+
+    if(RegisterClass(&wndClass))
+    {
+        HWND hhWindow = CreateWindow(
+                            wndClass.lpszClassName,
+                            TEXT("HH First Window"),
+                            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+                            CW_USEDEFAULT,
+                            CW_USEDEFAULT,
+                            CW_USEDEFAULT,
+                            CW_USEDEFAULT,
+                            0,
+                            0,
+                            hInstance,
+                            0);
+        
+        if(hhWindow)
+        {
+            MSG msg;
+            while(GetMessage(&msg, 0, 0, 0))    // hWnd has to NULL so that all msg in the process is fetched (not window specific)
+            {
+                TranslateMessage(&msg);
+                DispatchMessage(&msg);
+            }
+
+            return msg.wParam;
+        }
+        else
+        {
+            return -2;
+        }
+    }
+    else
+    {
+        return -1;
+    }
 }
